d9442101674e3276a4de575d2b9245f1118ff6dd
Cesar Peeters <cesar.peeters@gmail.com>
2019-11-01 18:29:40 +0100

    create internship/details.blade.php for views

--- resources/views/internship/details.blade.php
+++ resources/views/internship/details.blade.php
@@ -3,13 +3,23 @@
 @section('nav')
     <nav>
         <a href="/">Home</a>
-        <a href="#">Internships</a>
-        <a href="#">My Profile</a>
+        <a href="/internships">Internships</a>
+        <a href="/mystudent/profile">My Profile</a>
     </nav>
 @endsection
 
 @section('content')
 
-    <h1>Details stage</h1>
+    <h1>{{$internship->title}}</h1>
+    <h2>{{$internship->companyId}}</h2>
+    <p>Created - {{$internship->created_at->format('d/m/Y')}}</p>
+    <h3>Short summary of the internship</h3>
+    <p>{{$internship->description}}</p>
+    <h3>The skills you need to have</h3>
+    <p>{{$internship->requirements}}</p>
+    <h3>What the company offers</h3>
+    <p>{{$internship->offer}}</p>
+    
+    <button type="submit" class="btn btn-primary">Apply</button>
 
 @endsection
