0fd00273bbcaac046d4b5957b09204015cb3778d
Cesar Peeters <cesar.peeters@gmail.com>
2019-11-03 16:06:11 +0100

    add relationship Internship belongsTo Company

--- app/Internship.php
+++ app/Internship.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Internship extends Model
 {
-    //
+    public function company()
+    {
+        // Relations, een company heeft meerdere internships maar een internship behoort maar toe aan 1 company
+        return $this->belongsTo('\App\Company');
+    }
 }
