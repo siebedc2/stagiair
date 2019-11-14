e52b8a1d1d8f79f27dfbb5f27648c5910e2f09f8
Sam Ghyselinck <samlinck@outlook.com>
2019-11-14 20:45:15 +0100

    stage link fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,12 @@
     <div class="home">
         <h1>Welkom bij stagiair</h1>
         <h2>Jouw stage begint hier!</h2>
-        <div class="btn btn--stage">Zoek jouw stage!</div>
+        <div class="btn btn--stage"><a href="/stages">Zoek jouw stage!</a></div>
         <p>Stagiair is een platform dat stages op maat aanbied
             voor iedereen in België. We streven ernaar om een perfecte
             match te vinden tussen student en bedrijf. 
             Je avontuur start hier, waar wacht je nog op?</p>
     </div>
+    
     
 @endsection
