9f5ad3c410bdd7365cd5904993c41c467ddb0a3d
Siebe <siebedc2@gmail.com>
2019-12-07 15:59:50 +0100

    Fixed the logout for student en company in the HomeController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,13 @@
     <header class="stagiair_header">
         <a href="/" id="logo">Stagiair</a>
         <nav>
-            @if (Auth::check())
+            @if (Auth::guard('company')->check())
+                <a href="/stages">Stages zoeken</a>
+                <a href="/bedrijfsReviews">Bedrijfsreviews</a>
+                <a href="/mijnProfiel">{{Auth::guard('company')->user()->email}}</a>
+                <a href="/logout">Afmelden</a>
+
+            @elseif (Auth::check())
                 <a href="/stages">Stages zoeken</a>
                 <a href="/bedrijfsReviews">Bedrijfsreviews</a>
                 <a href="/mijnProfiel">{{Auth::user()->email}}</a>
