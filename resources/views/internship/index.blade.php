e95d1e656474bd904738e5dff5d932b75ab60b12
Bram Theunis <bramtheunis1997@gmail.com>
2019-12-08 19:35:24 +0100

    Added frontend form to make searching for internships possible

--- resources/views/internship/index.blade.php
+++ resources/views/internship/index.blade.php
@@ -6,6 +6,16 @@
 
     <h1>Stages</h1>
 
+    <form method="POST" action="{{URL::to('/search')}}" role="search" class="search">
+        {{ csrf_field() }}
+        <div class="input-group">
+            <input type="text" class="form-control" name="searchDescription" placeholder="Zoek stages">
+            <span class="input-group-btn">
+                <button type="submit" class="btn btn-search">Stages zoeken</button>
+            </span>
+        </div>
+    </form>
+
     @if( $flash = session('message') )
         <div class="alert alert-success">{{ $flash }}</div>
     @endif
