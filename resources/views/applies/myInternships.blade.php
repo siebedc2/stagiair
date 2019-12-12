cd476d5014900b163220cdaf694afbebc0dd7f77
Cesar Peeters <cesar.peeters@gmail.com>
2019-12-12 17:01:17 +0100

    add new view applies to show al te applies of a student

---
+++ resources/views/applies/myInternships.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts/app')
+
+@section('title')
+    Mijn sollicitaties
+@endsection
+
+@section('content')
+<h1>Mijn sollicitaties</h1>
+
+    @if( $flash = session('message') )
+        <div class="alert alert-success">{{ $flash }}</div>
+    @endif
+
+    <div class="container internships">
+        <div class="row">
+            <!-- lussen over $internships in de DB waar $i 1 internship is -->
+            @foreach ($student_internship as $i)
+                <a href="/stages/{{$i->id}}" class="internship">
+                    <!-- elk item van 1 internship in een html tag steken -->
+                    <h4 id="titleTeaser">{{$i->title}}</h4>
+                    <h4 id="companyTeaser">{{$i->company_id}}</h4>
+                    <i class="arrow right"></i>
+                    <p id="descriptionTeaser">{{$i->description}}</p>
+                </a>
+            @endforeach
+        </div>
+    </div>
+    
+       
+
+@endsection
