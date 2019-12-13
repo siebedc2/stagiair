fbc31dda8b4925daa178202748b7d6675e3cd3d9
Cesar Peeters <cesar.peeters@gmail.com>
2019-12-13 17:23:12 +0100

    fix apply on internships

--- app/Http/Controllers/ApplyController.php
+++ app/Http/Controllers/ApplyController.php
@@ -8,22 +8,16 @@ class ApplyController extends Controller
 {
     public function index()
     {
+        // eloquent query met relationships zodat student al de stages kan zien waar hij op gesolliciteerd heeft
         $student = \Auth::user();
-        $internships = $student->internships()->get();
-        dd($internships);
-
-        //if (\Auth::check()) {
-        // juiste data van internship uit db halen en in variable $data steken
-        $data['student_internship'] = \DB::table('student_internship')->where('student_id', \Auth::user()->id)->get();
+        // data megeven in array
+        $data['internships'] = $student->internships()->get();
 
         // returnen van de view index waar je de html en css gaat tonen + je geeft de $data mee als argument zodat je die data ook in de frontent kan gebruiken
         return view('applies/myInternships', $data);
-        //} else {
-        //return redirect('student/login');
-        //}
     }
 
-    public function store(Request $request, $internship)
+    public function store(Request $request)
     {
         // zorgen dat de message maar even blijft staan
         $request->flash();
@@ -31,13 +25,13 @@ class ApplyController extends Controller
         // data die ingegeven linken aan de db
         $student_internship = new \App\Apply();
         $student_internship->student_id = \Auth::user()->id;
-        $student_internship->internship_id = $internship;
+        $student_internship->internship_id = $request->input('internship_id');
         $student_internship->save();
 
         // flash message laten zien met een alert, deze blijft er maar even staan door -> flash()
         $request->session()->flash('message', 'Sollicitatie opgeslagen');
 
         // zorgen dat de juiste view wordt getoont
-        return view('applies/myInternships');
+        return redirect('/mijnProfiel/mijnSollicitaties');
     }
 }
